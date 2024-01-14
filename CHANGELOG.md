95b5af74399f4fe669102ebbdadf5569a2efedae
Benjamin Borbe <benjamin.borbe@gmail.com>
2024-01-14 11:20:56 +0100

    add iterator test

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -8,6 +8,10 @@ Please choose versions by [Semantic Versioning](http://semver.org/).
 * MINOR version when you add functionality in a backwards-compatible manner, and
 * PATCH version when you make backwards-compatible bug fixes.
 
+## v1.1.1
+
+- add test for iterator seek not found
+
 ## v1.1.0
 
 - Add context to update and view
--- kv_iterator-test-suite.go
+++ kv_iterator-test-suite.go
@@ -21,11 +21,11 @@ func IteratorTestSuite(ctx context.Context, db DB) {
 		Expect(err).To(BeNil())
 
 		{
-			err = bucket.Put(ctx, []byte("bk"), []byte("bv"))
+			err = bucket.Put(ctx, []byte("ck"), []byte("cv"))
 			Expect(err).To(BeNil())
 		}
 		{
-			err = bucket.Put(ctx, []byte("ak"), []byte("av"))
+			err = bucket.Put(ctx, []byte("bk"), []byte("bv"))
 			Expect(err).To(BeNil())
 		}
 
@@ -44,15 +44,78 @@ func IteratorTestSuite(ctx context.Context, db DB) {
 				Expect(err).To(BeNil())
 			}
 			Expect(keys).To(HaveLen(2))
-			Expect(keys[0]).To(Equal([]byte("ak")))
-			Expect(keys[1]).To(Equal([]byte("bk")))
+			Expect(keys[0]).To(Equal([]byte("bk")))
+			Expect(keys[1]).To(Equal([]byte("ck")))
 			Expect(values).To(HaveLen(2))
-			Expect(values[0]).To(Equal([]byte("av")))
-			Expect(values[1]).To(Equal([]byte("bv")))
+			Expect(values[0]).To(Equal([]byte("bv")))
+			Expect(values[1]).To(Equal([]byte("cv")))
 		}
 
 		{
 			it := bucket.Iterator()
+			defer it.Close()
+			var keys [][]byte
+			var values [][]byte
+			for it.Seek([]byte("ck")); it.Valid(); it.Next() {
+				item := it.Item()
+				keys = append(keys, item.Key())
+				err = item.Value(func(val []byte) error {
+					values = append(values, val)
+					return nil
+				})
+				Expect(err).To(BeNil())
+			}
+			Expect(keys).To(HaveLen(1))
+			Expect(keys[0]).To(Equal([]byte("ck")))
+			Expect(values).To(HaveLen(1))
+			Expect(values[0]).To(Equal([]byte("cv")))
+		}
+
+		{
+			it := bucket.Iterator()
+			defer it.Close()
+			var keys [][]byte
+			var values [][]byte
+			for it.Seek([]byte("ak")); it.Valid(); it.Next() {
+				item := it.Item()
+				keys = append(keys, item.Key())
+				err = item.Value(func(val []byte) error {
+					values = append(values, val)
+					return nil
+				})
+				Expect(err).To(BeNil())
+			}
+			Expect(keys).To(HaveLen(2))
+			Expect(keys[0]).To(Equal([]byte("bk")))
+			Expect(values[0]).To(Equal([]byte("bv")))
+			Expect(keys[1]).To(Equal([]byte("ck")))
+			Expect(values[1]).To(Equal([]byte("cv")))
+		}
+
+		{
+			it := bucket.IteratorReverse()
+			defer it.Close()
+			var keys [][]byte
+			var values [][]byte
+			for it.Rewind(); it.Valid(); it.Next() {
+				item := it.Item()
+				keys = append(keys, item.Key())
+				err = item.Value(func(val []byte) error {
+					values = append(values, val)
+					return nil
+				})
+				Expect(err).To(BeNil())
+			}
+			Expect(keys).To(HaveLen(2))
+			Expect(keys[0]).To(Equal([]byte("ck")))
+			Expect(keys[1]).To(Equal([]byte("bk")))
+			Expect(values).To(HaveLen(2))
+			Expect(values[0]).To(Equal([]byte("cv")))
+			Expect(values[1]).To(Equal([]byte("bv")))
+		}
+
+		{
+			it := bucket.IteratorReverse()
 			defer it.Close()
 			var keys [][]byte
 			var values [][]byte
@@ -76,7 +139,7 @@ func IteratorTestSuite(ctx context.Context, db DB) {
 			defer it.Close()
 			var keys [][]byte
 			var values [][]byte
-			for it.Rewind(); it.Valid(); it.Next() {
+			for it.Seek([]byte("dk")); it.Valid(); it.Next() {
 				item := it.Item()
 				keys = append(keys, item.Key())
 				err = item.Value(func(val []byte) error {
@@ -86,32 +149,13 @@ func IteratorTestSuite(ctx context.Context, db DB) {
 				Expect(err).To(BeNil())
 			}
 			Expect(keys).To(HaveLen(2))
-			Expect(keys[0]).To(Equal([]byte("bk")))
-			Expect(keys[1]).To(Equal([]byte("ak")))
+			Expect(keys[0]).To(Equal([]byte("ck")))
+			Expect(keys[1]).To(Equal([]byte("bk")))
 			Expect(values).To(HaveLen(2))
-			Expect(values[0]).To(Equal([]byte("bv")))
-			Expect(values[1]).To(Equal([]byte("av")))
+			Expect(values[0]).To(Equal([]byte("cv")))
+			Expect(values[1]).To(Equal([]byte("bv")))
 		}
 
-		{
-			it := bucket.IteratorReverse()
-			defer it.Close()
-			var keys [][]byte
-			var values [][]byte
-			for it.Seek([]byte("ak")); it.Valid(); it.Next() {
-				item := it.Item()
-				keys = append(keys, item.Key())
-				err = item.Value(func(val []byte) error {
-					values = append(values, val)
-					return nil
-				})
-				Expect(err).To(BeNil())
-			}
-			Expect(keys).To(HaveLen(1))
-			Expect(keys[0]).To(Equal([]byte("ak")))
-			Expect(values).To(HaveLen(1))
-			Expect(values[0]).To(Equal([]byte("av")))
-		}
 		return nil
 	})
 	Expect(err).To(BeNil())
